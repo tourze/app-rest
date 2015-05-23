eb47b9de88985e57c74900f4f646efece170ec71
luzhongpeng <1234qwqw>
2015-05-24 02:15:21 +0800

    增加一些缓存相关方法（未实现）

--- src/rest/storage/Base.php
+++ src/rest/storage/Base.php
@@ -5,6 +5,38 @@ namespace rest\storage;
 abstract class Base
 {
 
+    public $resourceName;
+
+    public $resourceID;
+
     public $meta;
 
+    /**
+     * 获取缓存
+     *
+     * @param $params
+     */
+    public function getCache($params)
+    {
+    }
+
+    /**
+     * 保存缓存
+     *
+     * @param $params
+     * @param $data
+     * @param $expire integer 过期时间
+     */
+    public function setCache($params, $data, $expire)
+    {
+    }
+
+    /**
+     * 强制删除缓存
+     *
+     * @param $params
+     */
+    public function deleteCache($params)
+    {
+    }
 }

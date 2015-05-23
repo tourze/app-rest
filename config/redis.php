a3fdf1a068b56b5cc30204671068c3f5c0d08df8
luzhongpeng <1234qwqw>
2015-05-24 02:39:51 +0800

    storage增加缓存方法，暂时只有读取

---
+++ config/redis.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'conn' => [
+        'scheme' => 'tcp',
+        'host'   => '127.0.0.1',
+        'port'   => 6379,
+    ],
+
+    'options' => [
+        'prefix' => 'rest:'
+    ],
+
+];
